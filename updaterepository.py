89002e08042aa48a350a19cc2cc3a131a3b2cddb
Pavithra2411 <66507672+Pavithra2411@users.noreply.github.com>
2020-12-17 12:31:59 +0530

    Update updaterepository.py

--- updaterepository.py
+++ updaterepository.py
@@ -1,5 +1,5 @@
 #Program to update description of a repository.
 import git
-repo = git.Repo('<the file directory of your local github repository>')
+repo = git.Repo('<the file directory of your local github repository>')#for example('Document/github/repositories')
 repo.update_discription:True
 repo.remotes.origin.pull()
